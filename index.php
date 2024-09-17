46614cd04cee4d9e9de36b3a88cb6efb0aa3eacd
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-09-17 18:38:59 +0200

    [UPD] bonus 1 completed

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
 <?php
-
+//definizione della classe movie
 class Movie {
     // variabili d'istanza
     public $title;
-    public $genre;
+    public $director;
     public $year;
-
-    public function __construct($_title, $_genre, $_year){
+    public $genres = [];
+    
+    //dichiaro il costruttore della classe
+    public function __construct($_title, $_director, $_year, $_genres = []){
         $this->title = $_title;
-        $this->genre = $_genre;
+        $this->director = $_director;
         $this->year = $_year;
+        //imposto i generi come un array
+        $this->genres = is_array($_genres) ? $_genres : [$_genres];
     }
 
     //metdodo che restituisce una descrizione della classe film
     public function getDescription() {
-        return "Il film " . $this->title . " è un film di genere " . $this->genre . ", uscito nel " . $this->year ;
+        //collego i generi separati dalle virgole
+        $genresStr = implode(", ", $this->genres);
+        return "Il film " . $this->title . " è diretto da " . $this->director . ", uscito nel " . $this->year . " ed appartiene ai generi: " . $genresStr . ".";
     }
 
 }
 
 // Istanziare due oggetti della classe Movie
-$movie1 = new Movie("Joker", "Thriller psicologico", 2019);
-$movie2 = new Movie("The Batman", "Supereroistico", 2022);
+$movie1 = new Movie("Joker", "Todd Phillips", 2019, ["drammatico", "poliziesco", "thriller", "noir"]);
+$movie2 = new Movie("The Batman", "Matt Reeves",2022, ["azione", "thriller", "noir", "drammatico", "poliziesco"]);
 
 // Stampare a schermo i valori delle proprietà e la descrizione
 echo $movie1->getDescription();
